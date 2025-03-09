5393aba2b6baac9f40cb192f1fe7b7bf8e3adaf6
leoli <leoli@yeahka.com>
2025-03-09 10:27:49 +0800

    feat: support for 404 error page

--- src/views/error/mobile.vue
+++ src/views/error/mobile.vue
@@ -1,7 +1,21 @@
-<script setup></script>
+<script setup>
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const handleClick = () => {
+  router.push("/");
+};
+</script>
 
 <template>
-  <h1>mobile error</h1>
+  <van-empty
+    description="页面被熊吃掉啦~"
+    image="/resources/images/errors/404-mobile.png"
+    :image-size="256"
+  >
+    <van-button round type="primary" icon="arrow-left" @click="handleClick"
+      >返回主页
+    </van-button>
+  </van-empty>
 </template>
-
-<style scoped lang="scss"></style>
